1e9b28c1d32b14319f9d43909e89e5440da0a1a6
andyvzcode <andresvz91@gmai.com>
2025-01-30 13:47:57 -0500

    Add: Styling inputs

--- project-manager-handler/src/App.jsx
+++ project-manager-handler/src/App.jsx
@@ -3,7 +3,7 @@ import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
 	return (
-		<main className="h-screen my-8 flex gap-8">
+		<main className="h-screen my-8 flex gap-8 ">
 			<ProjectsSidebar />
 			<NewProject />
 		</main>
--- project-manager-handler/src/components/Input.jsx
+++ project-manager-handler/src/components/Input.jsx
@@ -1,8 +1,16 @@
 export default function Input({ label, textarea, ...props }) {
+	const clasess =
+		"w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-500";
 	return (
-		<p>
-			<label>{label}</label>
-			{textarea ? <textarea {...props} /> : <input {...props} />}
+		<p className="flex flex-col gap-1 my-4">
+			<label className="text-sm font-bold uppercase text-stone-500">
+				{label}
+			</label>
+			{textarea ? (
+				<textarea className={clasess} {...props} />
+			) : (
+				<input className={clasess} {...props} />
+			)}
 		</p>
 	);
 }
